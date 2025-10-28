0330f97e3c4add7c9b76b2ae00d7b20e09d0e2c0
murodov109 <akabekov77@gmail.com>
2025-10-29 02:44:25 +0500

    Change BOT_TOKEN in config.py
    Updated the BOT_TOKEN to a new value.

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = "8245636373:AAFlLB0TAUYpBtTFjvVHnZr7jax7HbBNdGg"
+BOT_TOKEN = "8435705646:AAEJULzHr7N2Z-lleYsoQKf36yVnHxd0iXU"
 ADMIN_ID = 7617397626
 CHANNELS = ["@fantastik_tarjima_filmlar_hd", "@kerakli_xizmatlarn1"]
